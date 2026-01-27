3d5c1ae6e97e5232a04e47a850619262d47f2e7b
Ryan O'Connor <ryanoconnor@ROConnor-JNR64XGCHQ.local>
2026-01-27 09:16:06 -0800

    Remove ignore errors to find bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
